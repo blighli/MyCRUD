ec97cd2c07f5eac7fcf2a4f272054b3698f2671f
liqilei <blighli@gmail.com>
2024-03-06 20:28:15 +0800

    add post user api

--- src/main/java/com/liqilei/mycrud/controller/UserController.java
+++ src/main/java/com/liqilei/mycrud/controller/UserController.java
@@ -3,9 +3,9 @@ package com.liqilei.mycrud.controller;
 import com.liqilei.mycrud.entity.User;
 import com.liqilei.mycrud.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -18,4 +18,14 @@ public class UserController {
         return repo.findAll();
     }
 
+    @PostMapping
+    User add(@RequestBody User user) {
+        repo.save(user);
+        return user;
+    }
+
+    @GetMapping("/{id}")
+    Optional<User> get(@PathVariable long id) {
+        return repo.findById(id);
+    }
 }
